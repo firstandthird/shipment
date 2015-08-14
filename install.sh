f83fdc01e5573fffb070db5f43d7a8f25873516c
Greg Allen <me@jga.me>
2015-08-14 07:07:08 -0700

    don't auto install make, git, docker

--- install.sh
+++ install.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-apt-get update
-apt-get install -y git make curl
-curl -sSL https://get.docker.io/ubuntu/ | sh
-
 mkdir /tmp/shipment
 cd /tmp/shipment
 curl -L# https://github.com/firstandthird/shipment/archive/master.tar.gz | tar zx --strip 1
